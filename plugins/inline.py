1fe6ef57f2e99eb4b44ba8d81576f9b1308f6ec9
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2022-12-04 17:23:07 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -70,7 +70,7 @@ async def answer(bot, query):
                 title=file.file_name,
                 document_file_id=file.file_id,
                 caption=f_caption,
-                url=f'https://t.me/cyclonickfilterbot?start=files_{file.file_id}',
+                url=https://t.me/cyclonickfilterbot?start=files_{file.file_id},
                 description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
                 reply_markup=reply_markup))
 
